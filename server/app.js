51afa61680f831ed583e5376cb652d4aa190c35c
PhilRSmith <theonepudgypanda@gmail.com>
2020-02-27 12:12:36 -0500

    renamed folders appropriately

--- server/app.js
+++ server/app.js
@@ -4,4 +4,4 @@ const port = 3000 // variable to store the port to listen on
 
 app.get('/', (req, res) => res.send('Hello World!')) // IF you get a GET on '/' send 'Hello World' as a response'
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // make app listen on the port.
+app.listen(port, () => console.log(`app listening on port ${port}`)) // make app listen on the port.
