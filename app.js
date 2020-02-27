9f6ff3a01dc5a7ee953f2b859ca4372d6d4aa956
PhilRSmith <theonepudgypanda@gmail.com>
2020-02-27 13:33:21 -0500

    can now run to localhost:3000 using skele

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express') // IMPORT express
-const app = express() // create an instance of the import.
-const port = 3000 // variable to store the port to listen on
-
-app.get('/', (req, res) => res.send('Hello World!')) // IF you get a GET on '/' send 'Hello World' as a response'
-
-app.listen(port, () => console.log(`app listening on port ${port}`)) // make app listen on the port.
