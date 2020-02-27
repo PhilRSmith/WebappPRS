1406f6ece4ec33bce4733893b734298e664c5e7c
PhilRSmith <theonepudgypanda@gmail.com>
2020-02-27 09:17:57 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express') // IMPORT express
+const app = express() // create an instance of the import.
+const port = 3000 // variable to store the port to listen on
+
+app.get('/', (req, res) => res.send('Hello World!')) // IF you get a GET on '/' send 'Hello World' as a response'
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // make app listen on the port.
