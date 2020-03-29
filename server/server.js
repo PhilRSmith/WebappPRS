ec4a7be1a57e10774511d872218e1ab03a43af15
PhilRSmith <theonepudgypanda@gmail.com>
2020-03-29 11:26:27 -0400

    added demo s3 files/environments

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-
+var awsCtrl = require("./awsCtrl");
 var MongoClient = require('mongodb').MongoClient
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminLoginCredentials = 'mongodb+srv://PRSmith:Ocelot2893!@418term-ham3w.mongodb.net/test?retryWrites=true&w=majority'
 var app = express();
 
-
+require('dotenv').config();
   
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/sign_s3', awsCtrl);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
