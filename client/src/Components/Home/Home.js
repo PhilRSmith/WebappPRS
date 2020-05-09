72cab0d0b9fb96fb7f9d583fa467508e407e07fd
PhilRSmith <theonepudgypanda@gmail.com>
2020-05-09 10:55:14 -0400

    added temp covid info api to homepage

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -4,18 +4,49 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton } from 'react-twitter-embed';
 //import { Router, Switch, Route, Link } from 'react-router-dom'
 import DynamicCards from './DynamicCards'
+var unirest = require("unirest");
 
 class Home extends React.Component {
 	_isMounted = false;
 	constructor(props){
 		super(props);
 		this.state = { 
+			covidUSA: '',
 			cardData: []
 		}
 	};
 	
 		 
 	
+
+	covidInfo = () => {
+		var unirest = require("unirest");
+
+		var req = unirest("GET", "https://covid-19-data.p.rapidapi.com/country");
+
+		req.query({
+			"format": "json",
+			"name": "USA"
+		});
+
+		req.headers({
+			"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+			"x-rapidapi-key": "a8f18241b9mshf561d1f6fbaa6a0p13eefejsn0e47bbce602c"
+		})
+
+		.then(res => {
+			console.log(res)
+			var covidUSAInfo = res.body[0]
+			
+			this.setState({covidUSA: covidUSAInfo})
+		})	
+		
+		}
+	
+
+
+
+
 	homeLoadHandler = () =>  {
 		var url = `${this.props.baseUrl}/homecards`
 	 fetch(url)
@@ -65,11 +96,27 @@ class Home extends React.Component {
 						</div>
 					</Col>
 					<Col xs={12} sm={6} md={6} lg={6}>
+					<Row>
 						<div className = 'container' >
 							<div className = 'text-center'><h2 style = {headerStyle}> OUR LATEST ISSUE!!!</h2></div>
 							<DynamicCards passDataToDynamicCards = {this.state.cardData}/>		
-						</div>				
+						</div>	
+					</Row>
+					<div className = 'container' >
+						<div className = 'text-center'><h2 style = {headerStyle}> Covid Information</h2></div>
+						<Row>
+							<Col xs={7} sm={7} md={7} lg={7}><h3>Click button for info => </h3></Col>
+							<Col xs={5} sm={5} md={5} lg={5}><button type="button" className="btn btn-primary" onClick={this.covidInfo}> Covid Update</button></Col>
+						</Row>
+							<h5>{`Country: ${this.state.covidUSA.country}`}</h5>
+							<h5>{`Confirmed: ${this.state.covidUSA.confirmed}`}</h5>
+							<h5>{`Critical Condition: ${this.state.covidUSA.critical}`}</h5>
+							<h5>{`Deaths: ${this.state.covidUSA.deaths}`}</h5>
+							<h5>{`Recovered: ${this.state.covidUSA.recovered}`}</h5>
+						
+					</div>			
 					</Col>
+					
 				</Row>
 			</div>
 		</React.Fragment>
